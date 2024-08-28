e5dcb9b96706d907c4e5f455085ff5bf9e6b9167
Huan <98620169+bentonaw@users.noreply.github.com>
2024-08-28 22:37:31 +0200

    add models

---
+++ TableBookingSystem/Data/TableBookingSystemContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TableBookingSystem.Models;
+
+namespace TableBookingSystem.Data
+{
+	public class TableBookingSystemContext : DbContext
+	{
+        public TableBookingSystemContext(DbContextOptions<TableBookingSystemContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<TableReservation> TableReservations { get; set; }
+        public DbSet<TimeSlot> TimeSlot { get; set; }
+    }
+}
