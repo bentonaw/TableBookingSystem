7ff78ef625bb3f3ee7ec086e85a5d4f88b2d7ec2
Huan <98620169+bentonaw@users.noreply.github.com>
2024-08-29 00:42:26 +0200

    Add models

--- TableBookingSystem/Models/TableReservation.cs
+++ TableBookingSystem/Models/TableReservation.cs
@@ -1,6 +1,15 @@
-﻿namespace TableBookingSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TableBookingSystem.Models
 {
 	public class TableReservation
 	{
-	}
+        [Key]
+        public int TableReservationId { get; set; }
+        [ForeignKey("Reservation")]
+        public int ReservationId { get; set; }
+        [ForeignKey("Table")]
+        public int TableId { get; set; }
+    }
 }
