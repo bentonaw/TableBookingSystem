ce836791dec1b83a9592128024950574b1ac05d9
Huan <98620169+bentonaw@users.noreply.github.com>
2024-08-28 22:37:02 +0200

    add customermodel

---
+++ TableBookingSystem/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Mail;
+
+namespace TableBookingSystem.Models
+{
+	public class Customer
+	{
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
--- TableBookingSystem/Program.cs
+++ TableBookingSystem/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using TableBookingSystem.Data;
+
 namespace TableBookingSystem
 {
 	public class Program
@@ -6,6 +9,11 @@ namespace TableBookingSystem
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddDbContext<TableBookingSystemContext>(options =>
+			{
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+			});
 
 			// Add services to the container.
 
