4918fe75d1933fe96a221c8c7c2bd38cebd5d87b
Huan <98620169+bentonaw@users.noreply.github.com>
2024-08-29 02:09:38 +0200

    Add CustomerService with ICustomerService

---
+++ TableBookingSystem/Services/CustomerService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using TableBookingSystem.Data.Repo.IRepo;
+using TableBookingSystem.Models;
+using TableBookingSystem.Models.DTOs;
+using TableBookingSystem.Services.IService;
+
+namespace TableBookingSystem.Services
+{
+	public class CustomerService : ICustomerService
+	{
+		private readonly ICustomerRepo _customerRepo;
+		private readonly IMapper _mapper;
+        public CustomerService(ICustomerRepo customerRepo, IMapper mapper)
+        {
+			_customerRepo = customerRepo;
+			_mapper = mapper;
+        }
+        public async Task AddCustomersAsync(CustomerDTO customer)
+		{
+			var newCustomer = _mapper.Map<Customer>(customer);
+			await _customerRepo.AddCustomersAsync(newCustomer);
+		}
+
+		public async Task DeleteCustomerAsync(int customerId)
+		{
+			var customer = await _customerRepo.GetCustomerByIdAsync(customerId);
+			if (customer != null)
+			{
+				_customerRepo.DeleteCustomerAsync(customerId);
+			}
+		}
+		public async Task UpdateCustomerAsync(CustomerDTO customer)
+		{
+			var updatedCustomer = _mapper.Map<Customer>(customer);
+			await _customerRepo.UpdateCustomerAsync(updatedCustomer);
+		}
+
+		public async Task<IEnumerable<CustomerDTO>> GetAllCustomersAsync()
+		{
+			var customersList = await _customerRepo.GetAllCustomersAsync();
+			return _mapper.Map<IEnumerable<CustomerDTO>>(customersList);
+		}
+
+		public async Task<CustomerDTO> GetCustomerByIdAsync(int customerId)
+		{
+			var customer = await _customerRepo.GetCustomerByIdAsync(customerId);
+			return customer != null ? _mapper.Map<CustomerDTO>(customer) : null;
+		}
+
+		
+	}
+}
---
+++ TableBookingSystem/Services/IService/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using TableBookingSystem.Models.DTOs;
+
+namespace TableBookingSystem.Services.IService
+{
+	public interface ICustomerService
+	{
+		Task<IEnumerable<CustomerDTO>> GetAllCustomersAsync();
+		Task<CustomerDTO> GetCustomerByIdAsync(int customerId);
+		Task AddCustomersAsync(CustomerDTO customer);
+		Task UpdateCustomerAsync(CustomerDTO customer);
+		Task DeleteCustomerAsync(int customerId);
+	}
+}
