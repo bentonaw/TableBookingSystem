909f74848a6442713eab7b7ebacd953c9796f22a
Huan <98620169+bentonaw@users.noreply.github.com>
2024-08-29 02:10:04 +0200

    Add CustomerController

---
+++ TableBookingSystem/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TableBookingSystem.Models.DTOs;
+using TableBookingSystem.Services.IService;
+
+namespace TableBookingSystem.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomerController : ControllerBase
+	{
+		
+		private readonly ICustomerService _customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+			_customerService = customerService;
+        }
+
+		//Task<IEnumerable<CustomerDTO>> GetAllCustomersAsync();
+		//Task<CustomerDTO> GetCustomerByIdAsync(int customerId);
+		//Task AddCustomersAsync(CustomerDTO customer);
+		//Task UpdateCustomerAsync(CustomerDTO customer);
+		//Task DeleteCustomerAsync(int customerId);
+
+		[HttpPost]
+		public async Task<ActionResult> CreateCustomer([FromBody] CustomerDTO customer)
+		{
+			await _customerService.AddCustomersAsync(customer);
+			return Ok();
+		}
+		[HttpDelete]
+		[HttpPut]
+		[HttpGet]
+		[HttpGet]
+
+	}
+}
