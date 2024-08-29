360fddc0a6b5a3f3faaa998661127aecffdf4a39
Huan <98620169+bentonaw@users.noreply.github.com>
2024-08-29 21:58:11 +0200

    add customercontroller

--- TableBookingSystem/Controllers/CustomerController.cs
+++ TableBookingSystem/Controllers/CustomerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TableBookingSystem.Models.DTOs;
+using TableBookingSystem.Models.ViewModels;
 using TableBookingSystem.Services.IService;
 
 namespace TableBookingSystem.Controllers
@@ -15,22 +16,76 @@ namespace TableBookingSystem.Controllers
 			_customerService = customerService;
         }
 
-		//Task<IEnumerable<CustomerDTO>> GetAllCustomersAsync();
-		//Task<CustomerDTO> GetCustomerByIdAsync(int customerId);
-		//Task AddCustomersAsync(CustomerDTO customer);
-		//Task UpdateCustomerAsync(CustomerDTO customer);
-		//Task DeleteCustomerAsync(int customerId);
 
-		[HttpPost]
+		[HttpPost("createcustomer")]
 		public async Task<ActionResult> CreateCustomer([FromBody] CustomerDTO customer)
 		{
-			await _customerService.AddCustomersAsync(customer);
-			return Ok();
+			try
+			{
+				await _customerService.AddCustomersAsync(customer);
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
 		}
-		[HttpDelete]
-		[HttpPut]
-		[HttpGet]
-		[HttpGet]
+		[HttpDelete("deletecustomer")]
+		public async Task<ActionResult> DeleteCustomer(int customerId)
+		{
+			try
+			{
+				await _customerService.DeleteCustomerAsync(customerId);
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+		[HttpPut("updatecustomer")]
+		public async Task<ActionResult> UpdateCustomerInfo([FromBody] CustomerDTO customer)
+		{
+			try
+			{
+				await _customerService.UpdateCustomerAsync(customer);
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+		[HttpGet("getallcustomers")]
+		public async Task<ActionResult<IEnumerable<CustomerViewModel>>> ViewAllCustomers(CustomerViewModel customer)
+		{
+			try
+			{
+				var listOfCustomers = await _customerService.GetAllCustomersAsync();
+				return Ok(listOfCustomers);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
+		[HttpGet("getcustomer")]
+		public async Task<ActionResult> GetCustomerById(CustomerDTO customerDTO)
+		{
+			try
+			{
+				var customer = await _customerService.GetCustomerByIdAsync(customerDTO.CustomerId);
+				if (customerDTO == null)
+				{
+					return NotFound();
+				}
+				return Ok(customer);
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, $"An error occurred: {ex.Message}");
+			}
+		}
 
 	}
 }
