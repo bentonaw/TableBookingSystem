d5fcd47afb95c0e13fcb57bab193e24043804bd5
Huan <98620169+bentonaw@users.noreply.github.com>
2024-08-29 21:57:41 +0200

    Add customerdto and viewmodel

--- TableBookingSystem/Models/ViewModels/CustomerViewModel.cs
+++ TableBookingSystem/Models/ViewModels/CustomerViewModel.cs
@@ -1,8 +1,7 @@
-﻿namespace TableBookingSystem.Models.DTOs
+﻿namespace TableBookingSystem.Models.ViewModels
 {
-	public class CustomerDTO
+	public class CustomerViewModel
 	{
-		public int CustomerId { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string Email { get; set; }
