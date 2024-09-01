ef54dc3f7ba57f8ad960fe66bbb559ab3b823047
Huan <98620169+bentonaw@users.noreply.github.com>
2024-09-01 20:32:15 +0200

    fix data

---
+++ TableBookingSystem/Data/Repo/IRepo/IReservationRepo.cs
@@ -0,0 +1,15 @@
+﻿using TableBookingSystem.Models;
+
+namespace TableBookingSystem.Data.Repo.IRepo
+{
+	public interface IReservationRepo
+	{
+		//Task<IEnumerable<Reservation>> GetReservationsByDateAsync(DateTime date);
+		Task<IEnumerable<Reservation>> GetReservationsByDateRangeAsync(DateTime startDate, DateTime endDate);
+		Task<IEnumerable<Reservation>> GetReservationsByCustomerIdAsync(int customerId);
+		Task<Reservation> GetReservationByIdAsync(int reservationId);
+		Task AddReservationAsync(Reservation reservation);
+		Task UpdateReservationAsync(Reservation reservation);
+		Task DeleteReservationAsync(int reservationId);
+	}
+}
---
+++ TableBookingSystem/Data/Repo/ReservationRepo.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TableBookingSystem.Data.Repo.IRepo;
+using TableBookingSystem.Models;
+
+namespace TableBookingSystem.Data.Repo
+{
+	public class ReservationRepo : IReservationRepo
+	{
+		private readonly TableBookingSystemContext _context;
+        public ReservationRepo(TableBookingSystemContext context)
+        {
+			_context = context;
+        }
+        public async Task AddReservationAsync(Reservation reservation)
+		{
+			await _context.Reservations.AddAsync(reservation);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task DeleteReservationAsync(int reservationId)
+		{
+			var reservation = await _context.Reservations.FindAsync(reservationId);
+			if (reservation != null)
+			{
+				_context.Reservations.Remove(reservation);
+			}
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task<Reservation> GetReservationByIdAsync(int reservationId)
+		{
+			var reservation = await _context.Reservations.FindAsync(reservationId);
+			return reservation;
+		}
+
+		public async Task<IEnumerable<Reservation>> GetReservationsByCustomerIdAsync(int customerId)
+		{
+			var reservations = await _context.Reservations
+				.Where(r => r.Customer.CustomerId == customerId)
+				.ToListAsync();
+			return reservations;
+		}
+
+		//public Task<IEnumerable<Reservation>> GetReservationsByDateAsync(DateTime date)
+		//{
+		//	var reservations = await _context.Reservations.FindAsync(reservationId);
+		//	return reservations;
+		//}
+
+		public async Task<IEnumerable<Reservation>> GetReservationsByDateRangeAsync(DateTime startDate, DateTime endDate)
+		{
+			var reservations = await _context.Reservations
+				.Where(r => r.ReservationDate >= startDate && r.ReservationDate <= endDate)
+				.ToListAsync();
+			return reservations;
+		}
+
+		public async Task UpdateReservationAsync(Reservation reservation)
+		{
+			_context.Reservations.Update(reservation);
+			await _context.SaveChangesAsync();
+		}
+	}
+}
