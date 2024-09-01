d0c461bda8ae4415e38557938f02652eacf94dff
Huan <98620169+bentonaw@users.noreply.github.com>
2024-09-01 20:32:39 +0200

    fix services

---
+++ TableBookingSystem/Services/IService/IReservationService.cs
@@ -0,0 +1,16 @@
+﻿using TableBookingSystem.Models;
+using TableBookingSystem.Models.DTOs;
+using TableBookingSystem.Models.ViewModels;
+
+namespace TableBookingSystem.Services.IService
+{
+	public interface IReservationService
+	{
+		Task<IEnumerable<ReservationViewModel>> GetReservationsByDateRangeAsync(DateTime startDate, DateTime endDate);
+		Task<IEnumerable<ReservationViewModel>> GetReservationsByCustomerIdAsync(int customerId);
+		Task<ReservationViewModel> GetReservationByIdAsync(int reservationId);
+		Task AddReservationAsync(ReservationDTO reservationDTO);
+		Task UpdateReservationAsync(int reservationId, ReservationDTO reservationDTO);
+		Task DeleteReservationAsync(int reservationId);
+	}
+}
