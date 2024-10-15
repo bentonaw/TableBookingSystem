68cb962eea6ffa749026477a62fb0f3f56b9dd88
Huan <98620169+bentonaw@users.noreply.github.com>
2024-10-15 02:26:41 +0200

    fix menu and menuitems models

---
+++ TableBookingSystem/Models/DTOs/CreateMenuDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TableBookingSystem.Models.DTOs
+{
+    public class CreateMenuDTO
+    {
+        public int MenuId { get; set; }
+        public string MenuName { get; set; }
+        public ICollection<MenuItem> MenuItems { get; set; }
+    }
+}
